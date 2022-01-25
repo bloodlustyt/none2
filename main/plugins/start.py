b9b9132909cf04ce800b7b31bc97f78a865682b7
Mahesh Chauhan <84653476+vasusen-code@users.noreply.github.com>
2022-01-25 20:24:55 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -19,10 +19,9 @@ async def start(event):
                       buttons=[
                               [Button.inline("SET THUMB", data="sett"),
                                Button.inline("REM THUMB", data="remt")],
-                              [Button.inline("START BOT", data="startbot"),
-                               Button.inline("STOP BOT", data="stopbot")],
                               [Button.inline("LOG-IN", data="login"),
                                Button.inline("LOG-OUT", data="logout")],
+                              [Button.inline("Help.", data="help")],
                               ])
     tag = f'[{event.sender.first_name}](tg://user?id={event.sender_id})'
     await event.client.send_message(int(ACCESS), f'{tag} started the BOT\nUserID: {event.sender_id}') 
