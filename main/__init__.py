69115059115e854f592c075027321d259408b94e
bloodlustyt <67737806+bloodlustyt@users.noreply.github.com>
2022-11-20 21:30:39 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -11,10 +11,10 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 # variables
 API_ID = 3599592
 API_HASH = "80865dfca1e192f81931cbf61203cfe7"
-BOT_TOKEN = "5851808236:AAEc8zGr6Rd1EKbN5x3kAXXICMQepT24mJY"
-FORCESUB = 1485129059
-ACCESS = 630269331
-MONGODB_URI = "mongodb+srv://psychopomp:Cracker@1000@psychopomp.fgbjrip.mongodb.net/?retryWrites=true&w=majority"
-AUTH_USERS = 1058482162
+BOT_TOKEN = "5147519086:AAE4BKvnVw5fta_zqwDI7BhdkQv-7nmuWm4"
+FORCESUB = 1720432898
+ACCESS = 890533755
+MONGODB_URI = "mongodb+srv://Vasusen:darkmaahi@cluster0.o7uqb.mongodb.net/cluster0?retryWrites=true&w=majority"
+AUTH_USERS = 5351121397
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
